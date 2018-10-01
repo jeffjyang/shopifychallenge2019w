d4bd863a7351a6b75c2bee473c27cb969abd6694
Jeff Yang <jeffjieyang@gmail.com>
2018-10-01 02:04:25 -0400

    clean up code

--- src/main/java/com/jeffyang/shopifychallenge/guice/ShopifyChallengeGuiceModule.java
+++ src/main/java/com/jeffyang/shopifychallenge/guice/ShopifyChallengeGuiceModule.java
@@ -1,13 +1,11 @@
 package com.jeffyang.shopifychallenge.guice;
 
-import com.google.api.control.ServiceManagementConfigFilter;
 import com.google.api.control.extensions.appengine.GoogleAppEngineControlFilter;
 import com.google.api.server.spi.guice.EndpointsModule;
 import com.google.common.collect.ImmutableList;
 import com.googlecode.objectify.ObjectifyFilter;
 import com.jeffyang.shopifychallenge.api.OrderApi;
 import com.jeffyang.shopifychallenge.api.ShopApi;
-import com.jeffyang.shopifychallenge.api.ShopifyChallengeApiBase;
 
 import javax.inject.Singleton;
 import java.util.HashMap;
@@ -19,11 +17,9 @@ public class ShopifyChallengeGuiceModule extends EndpointsModule {
   public void configureServlets() {
     super.configureServlets();
 
-//    bind(ServiceManagementConfigFilter.class).in(Singleton.class);
     bind(ObjectifyFilter.class).in(Singleton.class);
 
     filter("/*").through(ObjectifyFilter.class);
-//    filter("/_ah/api/*").through(ServiceManagementConfigFilter.class);
 
     Map<String, String> apiController = new HashMap<String, String>();
     apiController.put("endpoints.projectId", "jeffyang-shopifychallenge2019w");
